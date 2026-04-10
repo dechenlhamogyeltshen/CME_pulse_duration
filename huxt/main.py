08d2ef042d4e3f79d3a203650dd55a1ac748552f
Dechen Lhamo Gyeltshen <dven@Dechens-MacBook-Pro.local>
2026-04-10 11:52:08 +0100

    Remove unused modules

--- huxt/main.py
+++ huxt/main.py
@@ -6,7 +6,6 @@ from datetime import datetime, timedelta
 from astropy.time import Time
 import re
 from sunpy.coordinates import sun
-import helio_coords as hcoords
 
 import joblib
 import onnxruntime as ort
